2ef80ba5b3c35e2b70767de31712b4c05e91ecee
Kajvix <100560130+Kajvix@users.noreply.github.com>
2022-03-03 13:20:57 +0100

    Add files via upload

---
+++ java4.js
@@ -0,0 +1,15 @@
+function stary1(){
+    document.getElementById("ch").innerHTML = "Rewolucja francuska, Wielka Rewolucja Francuska, także wielka rewolucja – okres w historii Francji w latach 1789–1799, w którym doszło do głębokich zmian polityczno-społecznych i obalenia monarchii Burbonów. Za jej symboliczny początek uważa się zdobycie Bastylii przez paryskich mieszczan 14 lipca 1789 roku (nieliczni wskazują na przysięgę deputowanych Stanów Generalnych, złożoną 20 czerwca 1789 roku w sali do gry w piłkę). Za koniec rewolucji uznaje się kres rządów dyrektoriatu 9 listopada (18 brumaire’a) 1799 roku, kiedy to Napoleon Bonaparte przeprowadził zamach stanu, ogłaszając się dzień później pierwszym konsulem. Podaje się także przewrót termidoriański z 27–28 lipca 1794 roku, kończący rządy jakobinów.";
+}
+
+function stary2(){
+    document.getElementById("ch").innerHTML = "Wielka Brytania, Zjednoczone Królestwo (ang. United Kingdom), Zjednoczone Królestwo Wielkiej Brytanii i Irlandii Północnej (ang. United Kingdom of Great Britain and Northern Ireland) – unitarne państwo wyspiarskie w Europie Zachodniej. W skład Wielkiej Brytanii zalicza się Anglia, Walia i Szkocja położone na wyspie Wielka Brytania oraz Irlandia Północna leżąca w północnej części wyspy Irlandia. Na wyspie tej znajduje się jedyna granica lądowa Zjednoczonego Królestwa z innym państwem – Irlandią. Poza nią Wielka Brytania otoczona jest przez Ocean Atlantycki na zachodzie i północy, Morze Północne na wschodzie, kanał La Manche na południu i Morze Irlandzkie na zachodzie.";
+}
+
+function stary3(){
+    document.getElementById("ch").innerHTML = "Ocean Indyjski – trzeci pod względem wielkości zbiornik wodny na świecie, zajmujący około 20% powierzchni Wszechoceanu. Oficjalna polska nazwa tego oceanu, zatwierdzona przez Komisję Standaryzacji Nazw Geograficznych brzmi Ocean Indyjski.";
+}
+
+function stary4(){
+    document.getElementById("ch").innerHTML = "Planeta – zgodnie z definicją Międzynarodowej Unii Astronomicznej – obiekt astronomiczny okrążający gwiazdę lub pozostałości gwiezdne, w którego wnętrzu nie zachodzą reakcje termojądrowe, wystarczająco duży, aby uzyskać prawie kulisty kształt oraz osiągnąć dominację w przestrzeni wokół swojej orbity. W odróżnieniu od gwiazd, świecących światłem własnym, planety świecą światłem odbitym.";
+}
